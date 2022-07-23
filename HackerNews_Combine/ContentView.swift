f8820212c791e38134d8b18810aac05f54fd7bdc
David Okonkwo <david.okonkwo@fueled.com>
2022-07-23 13:12:37 +0100

    Initial Commit

---
+++ HackerNews_Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews_Combine
+//
+//  Created by David Okonkwo on 23/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNews_Combine/HackerNews_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_CombineApp.swift
+//  HackerNews_Combine
+//
+//  Created by David Okonkwo on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
